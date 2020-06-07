351a59743491f849372efdd3e8d5cb447acf2ea6
SuperMario210 <Sup3rMar1o210@gmail.com>
2020-06-06 18:20:55 -0700

    added mnist dynamic models

--- cls/models.py
+++ cls/models.py
@@ -60,10 +60,10 @@ class LenetOptNet(nn.Module):
 
         assert(neq==0)
         self.M = Variable(torch.tril(torch.ones(nHidden, nHidden)).cuda())
-        self.L = Parameter(torch.tril(torch.rand(nHidden, nHidden).cuda()))
+        self.L = Parameter(torch.tril(torch.rand(nHidden, nHidden)).cuda()))
         self.G = Parameter(torch.Tensor(nineq,nHidden).uniform_(-1,1).cuda())
-        # self.z0 = Parameter(torch.zeros(nHidden).cuda())
-        # self.s0 = Parameter(torch.ones(nineq).cuda())
+        # self.z0 = Parameter(torch.zeros(nHidden.cuda())
+        # self.s0 = Parameter(torch.ones(nineq.cuda())
 
         self.nHidden = nHidden
         self.nineq = nineq
@@ -138,7 +138,7 @@ class OptNet(nn.Module):
 
         assert(neq==0)
         self.M = Variable(torch.tril(torch.ones(nCls, nCls)).cuda())
-        self.L = Parameter(torch.tril(torch.rand(nCls, nCls).cuda()))
+        self.L = Parameter(torch.tril(torch.rand(nCls, nCls)).cuda()))
         self.G = Parameter(torch.Tensor(nineq,nCls).uniform_(-1,1).cuda())
         self.z0 = Parameter(torch.zeros(nCls).cuda())
         self.s0 = Parameter(torch.ones(nineq).cuda())
@@ -212,5 +212,110 @@ class OptNetEq(nn.Module):
 
         x = QPFunction(verbose=False)(Q, p.double(), G, h, A, b).float()
         x = self.fc2(x)
+
+        return F.log_softmax(x)
+
+
+
+class LenetDynamic(nn.Module):
+    def __init__(self, nHidden=50, nineq=200, neq=0, eps=1e-4):
+        super(LenetDynamic, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, kernel_size=5)
+        self.conv2 = nn.Conv2d(20, 50, kernel_size=5)
+
+        self.qp_o = nn.Linear(50*4*4, nHidden)
+        self.qp_z0 = nn.Linear(50*4*4, nHidden)
+        self.qp_s0 = nn.Linear(50*4*4, nineq)
+
+        assert(neq==0)
+        self.M = Variable(torch.tril(torch.ones(nHidden, nHidden)).cuda())
+        self.L = Parameter(torch.tril(torch.rand(nHidden, nHidden)).cuda()))
+        self.G = Parameter(torch.Tensor(nineq,nHidden).uniform_(-1,1).cuda())
+        # self.z0 = Parameter(torch.zeros(nHidden.cuda())
+        # self.s0 = Parameter(torch.ones(nineq.cuda())
+
+        self.nHidden = nHidden
+        self.nineq = nineq
+        self.neq = neq
+        self.eps = eps
+
+    def forward(self, x):
+        nBatch = x.size(0)
+
+        x = F.max_pool2d(self.conv1(x), 2)
+        x = F.max_pool2d(self.conv2(x), 2)
+        x = x.view(nBatch, -1)
+
+        L = self.M*self.L
+        Q = L.mm(L.t()) + self.eps*Variable(torch.eye(self.nHidden)).cuda()
+        Q = Q.unsqueeze(0).expand(nBatch, self.nHidden, self.nHidden)
+        G = self.G.unsqueeze(0).expand(nBatch, self.nineq, self.nHidden)
+        z0 = self.qp_z0(x)
+        s0 = self.qp_s0(x)
+        h = z0.mm(self.G.t())+s0
+        e = Variable(torch.Tensor())
+        inputs = self.qp_o(x)
+        x = QPFunction(solver=QPSolvers.DYNAMIC_INEQ)(Q, inputs, G, h, e, e)
+        x = x[:,:10]
+
+        return F.log_softmax(x)
+
+class Dynamic(nn.Module):
+    def __init__(self, nFeatures, nHidden, nCls, bn, nineq=200, neq=0, eps=1e-4):
+        super().__init__()
+
+        self.nFeatures = nFeatures
+        self.nHidden = nHidden
+        self.bn = bn
+        self.nCls = nCls
+
+        if bn:
+            self.bn1 = nn.BatchNorm1d(nHidden)
+            self.bn2 = nn.BatchNorm1d(nCls)
+
+        self.fc1 = nn.Linear(nFeatures, nHidden)
+        self.fc2 = nn.Linear(nHidden, nCls)
+
+        # self.qp_z0 = nn.Linear(nCls, nCls)
+        # self.qp_s0 = nn.Linear(nCls, nineq)
+
+        assert(neq==0)
+        self.M = Variable(torch.tril(torch.ones(nCls, nCls)).cuda())
+        self.L = Parameter(torch.tril(torch.rand(nCls, nCls)).cuda()))
+        self.G = Parameter(torch.Tensor(nineq,nCls).uniform_(-1,1).cuda())
+        self.z0 = Parameter(torch.zeros(nCls).cuda())
+        self.s0 = Parameter(torch.ones(nineq).cuda())
+
+        self.nineq = nineq
+        self.neq = neq
+        self.eps = eps
+
+    def forward(self, x):
+        nBatch = x.size(0)
+
+        # FC-ReLU-(BN)-FC-ReLU-(BN)-QP-Softmax
+        x = x.view(nBatch, -1)
+        x = F.relu(self.fc1(x))
+        if self.bn:
+            x = self.bn1(x)
+        x = F.relu(self.fc2(x))
+        if self.bn:
+            x = self.bn2(x)
+
+        L = self.M*self.L
+        Q = L.mm(L.t()) + self.eps*Variable(torch.eye(self.nCls)).cuda()
+        Q = Q.unsqueeze(0).expand(nBatch, self.nCls, self.nCls)
+        G = self.G.unsqueeze(0).expand(nBatch, self.nineq, self.nCls)
+        # z0 = self.qp_z0(x)
+        # s0 = self.qp_s0(x)
+        z0 = self.z0.unsqueeze(0).expand(nBatch, self.nCls)
+        s0 = self.s0.unsqueeze(0).expand(nBatch, self.nineq)
+        h = z0.mm(self.G.t())+s0
+        e = Variable(torch.Tensor())
+        inputs = x
+        x = QPFunction(verbose=False, solver=QPSolvers.DYNAMIC_INEQ)(
+            Q.double(), inputs.double(), G.double(), h.double(), e, e)
+        x = x.float()
+        # x = x[:,:10].float()
 
         return F.log_softmax(x)
